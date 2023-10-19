af9743de400b97e366fc86603bdd1768153543e0
khaidz123321 <148494753+khaidz123321@users.noreply.github.com>
2023-10-20 03:06:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Nguyen Lan Huong 🌷';
+const contentCard = 'Chúc em 20/10 vui vẻ, ngày càng xinh đẹp hơn, học giỏi hơn, mãi bên anh, chúc em sau này cười nhiều hơn ,cảm ơn em đã bên cạnh anh, cảm ơn vì em đã đến, mãi là embe của anhhh, chuc em tat ca! 💝';
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '2711.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
